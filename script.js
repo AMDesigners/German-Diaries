5d60f87ff4a1c2d898f528ba55f99a3f7c0090cf
AMDesigners <85679843+AMDesigners@users.noreply.github.com>
2022-07-06 11:01:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,62 @@
 $(document).ready(() => {
-    $(".wrapper").addClass('main-active');
-    $(window).scroll(() => {
-        if (this.scrollY > 300) {
-            $(".main-col2, .main-col3").addClass('main-active');
-        }
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }
-        else {
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-    });
+  $(".wrapper").addClass("main-active");
+  $(window).scroll(() => {
+    if (this.scrollY > 150) {
+      $(".main-col2, .main-col3").addClass("main-active");
+    }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
 
-    // typing animation script
-    var typed = new Typed(".typing" , {
-        strings: ["It Is the Most Spoken Native Language in Europe.","It Has Great Cultural Inheritance and its Contribution to Knowledge is Priceless.","Excellent Business Opportunities.","Study at Renowned German Universities.","It Is Not as Hard as It Is Made Out to Be."],
-        typeSpeed: 60,
-        backSpeed: 30,
-        loop: true
-    });
+  // typing animation script
+  var typed = new Typed(".typing", {
+    strings: ["Learning<br>Is<br>Fun", "Learnen<br>Macht<br>Spaß"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+  var typed = new Typed(".typing-2", {
+    strings: [],
+    typeSpeed: 200,
+    backSpeed: 70,
+    loop: true,
+  });
+  var typed = new Typed(".typing-3", {
+    strings: [],
+    typeSpeed: 200,
+    backSpeed: 60,
+    loop: true,
+  });
 });
+
+let main = document.querySelector(".main-div");
+let bucket = document.querySelector(".bucket");
+let main1 = document.querySelector(".main-div1");
+let bucket1 = document.querySelector(".bucket1");
+let main2 = document.querySelector(".main-div2");
+let bucket2 = document.querySelector(".bucket2");
+
+let year = document.getElementById("year");
+year.innerText = new Date().getFullYear().toString();
+
+function load() {
+  main.style.display = "none";
+  bucket.style.display = "block";
+}
+
+function load1() {
+  main1.style.display = "none";
+  bucket1.style.display = "block";
+}
+
+function load2() {
+  main2.style.display = "none";
+  bucket2.style.display = "block";
+}
